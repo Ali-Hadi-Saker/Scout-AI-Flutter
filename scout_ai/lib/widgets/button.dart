2b5acc96c14f9bf24380eafb0f7439984353cef9
Ali-Hadi-Saker <alihady530@gmail.com>
2024-08-20 10:32:55 +0300

    updated button widget

--- scout_ai/lib/widgets/button.dart
+++ scout_ai/lib/widgets/button.dart
@@ -4,27 +4,31 @@ class CustomizedButton extends StatelessWidget {
   final String text;
   final VoidCallback onPressed;
   final double? width;
+  final Icon? suffexIcon;
   const CustomizedButton(
-      {super.key,
-      required this.text,
-      required this.onPressed,
-      this.width});
+      {super.key, required this.text, required this.onPressed, this.width, this.suffexIcon});
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: width ?? double.infinity,      
+      width: width ?? double.infinity,
       child: MaterialButton(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),        
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
           color: Colors.blue,
           onPressed: onPressed,
           child: Padding(
             padding: const EdgeInsets.only(bottom: 15, top: 15),
-            child: Text(
-              text,
-              style: TextStyle(
-                color: Colors.white, 
-                fontSize: 22),
+            child: Row(
+              children: [
+                Text(
+                  text,
+                  style: TextStyle(color: Colors.white, fontSize: 22),
+                ),
+                if(suffexIcon != null) ...[
+                  SizedBox(width: 30,),
+                  suffexIcon!
+                ]
+              ],
             ),
           )),
     );

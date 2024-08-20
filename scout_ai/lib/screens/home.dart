968ba35794b0028931866e0167f931c953c32f5b
Ali-Hadi-Saker <alihady530@gmail.com>
2024-08-20 10:53:20 +0300

    added project name to home screen

--- scout_ai/lib/screens/home.dart
+++ scout_ai/lib/screens/home.dart
@@ -26,11 +26,17 @@ class _HomeScreenState extends State<HomeScreen> {
                   image: AssetImage('assets/images/robot.png')),
               ),
             SizedBox(            
-              height: 200,
-              width: 200,
+              height: 180,
+              width: 180,
               child: Image(
                 image: AssetImage('assets/images/scout_ai_logo_only.png')),
                 ),
+                Text("Scout AI",
+                style: TextStyle(
+                  color: Color.fromRGBO(50,146,230, 1),
+                  fontSize: 28,
+                  fontWeight: FontWeight.w900
+                ),),
                 SizedBox(height: 70,),
             CustomizedButton(
               heigth: 100,

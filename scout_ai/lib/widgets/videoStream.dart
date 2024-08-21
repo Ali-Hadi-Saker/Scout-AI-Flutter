dc2c7faa9a249068f170ef1a67770ab85ff1ebdd
Ali-Hadi-Saker <alihady530@gmail.com>
2024-08-21 23:35:24 +0300

    updated videoSream widget to use video_player package

--- scout_ai/lib/widgets/videoStream.dart
+++ scout_ai/lib/widgets/videoStream.dart
@@ -1,21 +1,44 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_mjpeg/flutter_mjpeg.dart';
+import 'package:video_player/video_player.dart';
 
-class VideoSctream extends StatelessWidget {
+class VideoStream extends StatefulWidget {
   final String streamUrl;
-  const VideoSctream({super.key, required this.streamUrl});
+  const VideoStream({super.key, required this.streamUrl});
+
+  @override
+  _VideoStreamState createState() => _VideoStreamState();
+}
+
+class _VideoStreamState extends State<VideoStream> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.network(widget.streamUrl)
+      ..initialize().then((_) {
+        setState(() {});
+        _controller.play();
+      });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Mjpeg(
-      stream: streamUrl,
-      isLive: true,
-      error: (context, error, stackTrace) {
-        return Center(child: Text('Failed to load stream'));
-      },
-      loading: (context) {
-        return Center(child: CircularProgressIndicator());
-      },
+    return Scaffold(
+      body: Center(
+        child: _controller.value.isInitialized
+            ? AspectRatio(
+                aspectRatio: _controller.value.aspectRatio,
+                child: VideoPlayer(_controller),
+              )
+            : CircularProgressIndicator(),
+      ),
     );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
   }
 }

6a13684f9be0a01aa6ee9ac380c24331ac179d8c
Ali-Hadi-Saker <alihady530@gmail.com>
2024-08-21 16:55:58 +0300

    added videoStream widget

---
+++ scout_ai/lib/widgets/videoStream.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class VideoSctream extends StatefulWidget {
+  const VideoSctream({super.key});
+
+  @override
+  State<VideoSctream> createState() => _VideoSctreamState();
+}
+
+class _VideoSctreamState extends State<VideoSctream> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

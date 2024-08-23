4fddb168ba176b6f10be72c614c881f9deaf85ef
Ali-Hadi-Saker <alihady530@gmail.com>
2024-08-23 05:12:25 +0300

    tested video stream

--- scout_ai/lib/screens/test.dart
+++ scout_ai/lib/screens/test.dart
@@ -1,9 +1,73 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter_vlc_player/flutter_vlc_player.dart';
+// import 'package:screenshot/screenshot.dart';
+// import 'package:flutter_mjpeg/flutter_mjpeg.dart';
+
+
+
+// class TestScreen extends StatefulWidget {
+//   @override
+//   _TestScreenState createState() => _TestScreenState();
+// }
+
+// class _TestScreenState extends State<TestScreen> {
+//   ScreenshotController screenshotController = ScreenshotController();
+
+//   @override
+//   // void initState() {
+//   //   super.initState();
+//   //   _vlcPlayerController = VlcPlayerController.network(
+//   //     'http://192.168.0.107',  // Replace with the ESP32 IP address
+//   //     hwAcc: HwAcc.full,
+//   //     autoPlay: true,
+//   //     options: VlcPlayerOptions(),
+//   //   );
+//   // }
+
+//   @override
+//   // void dispose() {
+//   //   _vlcPlayerController.dispose();
+//   //   super.dispose();
+//   // }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('ESP32 Camera Stream'),
+//       ),
+//       body: Container(
+//           height: MediaQuery.of(context).size.width,
+//           width: MediaQuery.of(context).size.width,
+//           decoration: BoxDecoration(
+//               borderRadius:
+//                   BorderRadius.all(Radius.circular(10))),
+//           child: Screenshot(
+//             controller: screenshotController,
+//             child: Mjpeg(
+//   stream: 'http://172.20.10.3',
+//   isLive: true,
+//   error: (BuildContext context, dynamic error, dynamic stackTrace) {
+//     print("Error loading stream: $error");
+//     return Container(
+//       decoration: BoxDecoration(
+//           color: Colors.grey,
+//           borderRadius: BorderRadius.all(Radius.circular(10))),
+//       child: Center(
+//         child: Text(
+//           'No stream found',
+//           style: TextStyle(color: Colors.white),
+//         ),
+//       ),
+//     );
+//   },
+// ),
+//           )),
+//     );
+//   }
+// }
 import 'package:flutter/material.dart';
 import 'package:flutter_vlc_player/flutter_vlc_player.dart';
-import 'package:screenshot/screenshot.dart';
-import 'package:flutter_mjpeg/flutter_mjpeg.dart';
-
-
 
 class TestScreen extends StatefulWidget {
   @override
@@ -11,24 +75,23 @@ class TestScreen extends StatefulWidget {
 }
 
 class _TestScreenState extends State<TestScreen> {
-  ScreenshotController screenshotController = ScreenshotController();
+  late VlcPlayerController _vlcPlayerController;
 
   @override
-  // void initState() {
-  //   super.initState();
-  //   _vlcPlayerController = VlcPlayerController.network(
-  //     'http://192.168.0.107',  // Replace with the ESP32 IP address
-  //     hwAcc: HwAcc.full,
-  //     autoPlay: true,
-  //     options: VlcPlayerOptions(),
-  //   );
-  // }
+  void initState() {
+    super.initState();
+    _vlcPlayerController = VlcPlayerController.network(
+      'http://172.20.10.3',  // Correct URL and port
+      hwAcc: HwAcc.full,
+      autoPlay: true,
+    );
+  }
 
   @override
-  // void dispose() {
-  //   _vlcPlayerController.dispose();
-  //   super.dispose();
-  // }
+  void dispose() {
+    _vlcPlayerController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,33 +99,12 @@ class _TestScreenState extends State<TestScreen> {
       appBar: AppBar(
         title: Text('ESP32 Camera Stream'),
       ),
-      body: Container(
-          height: MediaQuery.of(context).size.width,
-          width: MediaQuery.of(context).size.width,
-          decoration: BoxDecoration(
-              borderRadius:
-                  BorderRadius.all(Radius.circular(10))),
-          child: Screenshot(
-            controller: screenshotController,
-            child: Mjpeg(
-  stream: 'http://172.20.10.3',
-  isLive: true,
-  error: (BuildContext context, dynamic error, dynamic stackTrace) {
-    print("Error loading stream: $error");
-    return Container(
-      decoration: BoxDecoration(
-          color: Colors.grey,
-          borderRadius: BorderRadius.all(Radius.circular(10))),
-      child: Center(
-        child: Text(
-          'No stream found',
-          style: TextStyle(color: Colors.white),
-        ),
+      body: VlcPlayer(
+        controller: _vlcPlayerController,
+        aspectRatio: 16 / 9,
+        placeholder: Center(child: CircularProgressIndicator()),
       ),
-    );
-  },
-),
-          )),
     );
   }
 }
+

13628aff269e6d3e64bf0ac0a51ae8d0b0a16fb3
Ali-Hadi-Saker <alihady530@gmail.com>
2024-08-24 21:45:13 +0300

    added appBar to settings screen

--- scout_ai/lib/screens/settings.dart
+++ scout_ai/lib/screens/settings.dart
@@ -10,6 +10,15 @@ class SettingScreen extends StatefulWidget {
 class _SettingScreenState extends State<SettingScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () => Navigator.pop(context),
+          icon: Icon(Icons.arrow_back)),
+        title:  Center(child: Text("User Details")),
+
+          ),
+      
+    );
   }
 }

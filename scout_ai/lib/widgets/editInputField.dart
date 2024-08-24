a4649c37aa739a776de5a5d0d8f87b0110fcb30d
Ali-Hadi-Saker <alihady530@gmail.com>
2024-08-25 00:34:44 +0300

    added style for editInput widget

--- scout_ai/lib/widgets/editInputField.dart
+++ scout_ai/lib/widgets/editInputField.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 
 class editInputField extends StatelessWidget {
-  const editInputField({super.key});
+  final String hintText;
+  const editInputField({super.key, required this.hintText});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return TextField(
+      decoration: InputDecoration(
+        hintText: hintText,
+        hintStyle: TextStyle(color: Colors.grey),
+        enabledBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color: Colors.grey),
+        ),
+        focusedBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color: Colors.blue),
+        ),
+      ),
+    );
   }
 }

32909f2df82798a1794dd626b15efb4c32d3adcd
Ali-Hadi-Saker <alihady530@gmail.com>
2024-08-24 12:38:35 +0300

    initiated bottomNavigation wiget

---
+++ scout_ai/lib/widgets/bottomNavigation.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BottomNavigatoin extends StatefulWidget {
+  const BottomNavigatoin({super.key});
+
+  @override
+  State<BottomNavigatoin> createState() => _BottomNavigatoinState();
+}
+
+class _BottomNavigatoinState extends State<BottomNavigatoin> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

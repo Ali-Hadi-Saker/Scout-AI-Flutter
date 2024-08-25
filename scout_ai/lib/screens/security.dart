9332fd8bb0b0d3679a2a5fff6552e910eabcbf90
Ali-Hadi-Saker <alihady530@gmail.com>
2024-08-25 13:01:57 +0300

    added needed variables

--- scout_ai/lib/screens/security.dart
+++ scout_ai/lib/screens/security.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:scout_ai/widgets/edit_input_field.dart';
 import 'package:scout_ai/widgets/screen_icon.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SecurityScreen extends StatefulWidget {
   const SecurityScreen({super.key});
@@ -10,7 +13,18 @@ class SecurityScreen extends StatefulWidget {
 }
 
 class _SecurityScreenState extends State<SecurityScreen> {
-  
+  String? currentPassword;
+  String? newPassword;
+  String? confirmNewPassword;
+  TextEditingController newPasswordController = TextEditingController();
+  TextEditingController confirmNewPasswordController = TextEditingController();
+  void loadUserData() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String userDataJson = prefs.getString('userData')!;
+
+    var userData = jsonDecode(userDataJson);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,11 +37,13 @@ class _SecurityScreenState extends State<SecurityScreen> {
       body: Column(
         children: [
           ScreenIcon(screenIon: Icon(Icons.shield_rounded)),
-          SizedBox(height: 100,),
+          SizedBox(
+            height: 100,
+          ),
           editInputField(
-            hintText: "Current Password", 
-            prefixIcon: Icon(Icons.shield_rounded), 
-            controller: controller)
+              hintText: "Current Password",
+              prefixIcon: Icon(Icons.shield_rounded),
+              controller: controller)
         ],
       ),
     );

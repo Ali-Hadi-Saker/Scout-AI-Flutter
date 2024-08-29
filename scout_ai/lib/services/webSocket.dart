736377e749a713bfbef3b34c7d45157d8db1e1ae
Ali-Hadi-Saker <alihady530@gmail.com>
2024-08-29 14:48:39 +0300

    created webSocketService class and established webSocket channel

---
+++ scout_ai/lib/services/webSocket.dart
@@ -0,0 +1,10 @@
+import 'package:web_socket_channel/web_socket_channel.dart';
+import 'package:flutter/foundation.dart';
+
+class WebSocketService {
+
+  final WebSocketChannel _channel;
+  
+  WebSocketService(String url) : _channel = WebSocketChannel.connect(Uri.parse(url));
+
+}

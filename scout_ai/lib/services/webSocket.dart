45601421e7c2049d21599fedd76aeb7a3d91d07f
Ali-Hadi-Saker <alihady530@gmail.com>
2024-08-29 14:51:16 +0300

    created send message function thru webSocket server

--- scout_ai/lib/services/webSocket.dart
+++ scout_ai/lib/services/webSocket.dart
@@ -2,9 +2,12 @@ import 'package:web_socket_channel/web_socket_channel.dart';
 import 'package:flutter/foundation.dart';
 
 class WebSocketService {
-
   final WebSocketChannel _channel;
-  
-  WebSocketService(String url) : _channel = WebSocketChannel.connect(Uri.parse(url));
 
+  WebSocketService(String url)
+      : _channel = WebSocketChannel.connect(Uri.parse(url));
+
+  void send(message) {
+    _channel.sink.add(message);
+  }
 }

1823b885226ac9308a46754972a34b5b7c552bf1
Ali-Hadi-Saker <alihady530@gmail.com>
2024-08-30 23:27:42 +0300

    modified video stream widget to statefulWidget

--- scout_ai/lib/widgets/video_stream.dart
+++ scout_ai/lib/widgets/video_stream.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mjpeg/flutter_mjpeg.dart';
 
-class VideoStream extends StatelessWidget {
+class VideoStream extends StatefulWidget {
   final String streamUrl;
   const VideoStream({super.key, required this.streamUrl});
 
   @override
+  State<VideoStream> createState() => _VideoStreamState();
+}
+
+class _VideoStreamState extends State<VideoStream> {
+  @override
   Widget build(BuildContext context) {
     return Mjpeg(
-      stream: streamUrl,
+      stream: widget.streamUrl,
       isLive: true,
       error: (context, error, stackTrace) {
         return Center(child: Text('Failed to load stream'));

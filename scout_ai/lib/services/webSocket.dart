634e9973956779c69d9a3edbfffad65c0673df87
Ali-Hadi-Saker <alihady530@gmail.com>
2024-08-31 00:00:46 +0300

    updated websocket services to handle binary video

--- scout_ai/lib/services/webSocket.dart
+++ scout_ai/lib/services/webSocket.dart
@@ -1,18 +1,28 @@
+import 'dart:async';
+import 'dart:typed_data';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
 class WebSocketService {
   final WebSocketChannel _channel;
+  final _controller = StreamController<Uint8List>();
 
   WebSocketService(String url)
-      : _channel = WebSocketChannel.connect(Uri.parse(url));
+      : _channel = WebSocketChannel.connect(Uri.parse(url)) {
+    _channel.stream.listen((data) {
+      if (data is Uint8List) {
+        _controller.add(data);
+      }
+    });
+  }
 
   void send(message) {
     _channel.sink.add(message);
   }
 
-  Stream<dynamic> get stream => _channel.stream;
+  Stream<Uint8List> get videoStream => _controller.stream;
 
   void dispose() {
+    _controller.close();
     _channel.sink.close();
   }
 }

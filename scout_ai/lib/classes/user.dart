ff98799975cae1caf187cee7c044d0cac2e08056
Ali-Hadi-Saker <alihady530@gmail.com>
2024-09-02 10:39:02 +0300

    added user factory constructor

--- scout_ai/lib/classes/user.dart
+++ scout_ai/lib/classes/user.dart
@@ -4,4 +4,12 @@ class User {
   final String email;
 
   User({required this.fname, required this.password, required this.email});
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      fname: json['fname'],
+      password: json['password'],
+      email: json['email'],
+    );
+}
 }

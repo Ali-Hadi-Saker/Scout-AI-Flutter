20a17121c6c60fbe8e4641820ced89cf4fade96d
Ali-Hadi-Saker <alihady530@gmail.com>
2024-09-15 18:33:57 +0300

    edited colors class to gradient colors

--- scout_ai/lib/utils/constant.dart
+++ scout_ai/lib/utils/constant.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primaryButtonColor = Color.fromARGB(255, 67, 123, 245); 
-  static const Color logoutButtonColor = Color.fromARGB(255, 240, 36, 36); 
+  static const LinearGradient primaryButtonGradient = LinearGradient(
+    colors: [
+      Color.fromARGB(255, 67, 123, 245),
+      Color.fromARGB(255, 45, 105, 235),
+    ],
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+  );
+
+  static const LinearGradient logoutButtonGradient = LinearGradient(
+    colors: [
+      Color.fromARGB(255, 240, 36, 36),
+      Color.fromARGB(255, 210, 20, 20),
+    ],
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+  );
 }

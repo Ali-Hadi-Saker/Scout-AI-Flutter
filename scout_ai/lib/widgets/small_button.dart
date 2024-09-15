fc5c50b7e3e0d96da9eae1e43a47c213a10adbf0
Ali-Hadi-Saker <alihady530@gmail.com>
2024-09-15 18:42:22 +0300

    updated small-button widget to accept gradient color

--- scout_ai/lib/widgets/small_button.dart
+++ scout_ai/lib/widgets/small_button.dart
@@ -2,30 +2,37 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SmallButton extends StatelessWidget {
-  final Color color;
+  final LinearGradient gradient;
   final String text;
   final VoidCallback onPressed;
   final bool isFullWidth;
-  const SmallButton(
-      {super.key,
-      required this.color,
-      required this.onPressed,
-      required this.text,
-      this.isFullWidth = false});
+
+  const SmallButton({
+    super.key,
+    required this.gradient,
+    required this.onPressed,
+    required this.text,
+    this.isFullWidth = false,
+  });
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       width: isFullWidth ? double.infinity : null,
-      child: MaterialButton(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-        color: color,
-        onPressed: onPressed,
-        child: Padding(
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          gradient: gradient, 
+        ),
+        child: MaterialButton(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8),
+          ),
+          onPressed: onPressed,
           padding: const EdgeInsets.all(15),
           child: Text(
             text,
-            style: TextStyle(color: Colors.white, fontSize: 20),
+            style: const TextStyle(color: Colors.white, fontSize: 20),
             textAlign: TextAlign.left,
           ),
         ),

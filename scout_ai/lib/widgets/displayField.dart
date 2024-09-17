6630a22418edee95c1e149cf87cb42aa444c5c4c
Ali-Hadi-Saker <alihady530@gmail.com>
2024-09-17 12:39:53 +0300

    initiated display-field

---
+++ scout_ai/lib/widgets/displayField.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class DisplayField extends StatelessWidget {
+  const DisplayField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

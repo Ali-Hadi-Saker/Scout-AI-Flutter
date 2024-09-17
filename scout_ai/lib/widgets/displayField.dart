4567ede4a3bdfb42adbc9399b31f47c5e5b89de4
Ali-Hadi-Saker <alihady530@gmail.com>
2024-09-17 12:55:43 +0300

    created display-field widget

--- scout_ai/lib/widgets/displayField.dart
+++ scout_ai/lib/widgets/displayField.dart
@@ -1,12 +1,34 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class DisplayField extends StatelessWidget {
-  final Text text;
+  final String text;
   final Icon preffixIcon;
-  const DisplayField({super.key,required this.text, required this.preffixIcon});
+  const DisplayField(
+      {super.key, required this.text, required this.preffixIcon});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Row(
+      children: [
+        preffixIcon,
+        SizedBox(width: 10,),
+        Column(
+          children: [
+            Text(
+              text,
+              style: const TextStyle(
+                      fontSize: 16,
+                      color: Colors.black87,  
+                    ),
+            ),
+            const Divider(
+                  color: Colors.grey, 
+                  thickness: 1,
+                ),
+          ],
+        )
+      ],
+    );
   }
 }
